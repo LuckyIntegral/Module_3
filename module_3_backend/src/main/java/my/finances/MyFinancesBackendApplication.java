27284df20235a938497bdd0935eee905d767af5f
Vitalik <frantsv2004@gmail.com>
2023-04-13 16:30:33 +0200

    first commit

---
+++ module_3_backend/src/main/java/my/finances/MyFinancesBackendApplication.java
@@ -0,0 +1,11 @@
+package my.finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinancesBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyFinancesBackendApplication.class, args);
+    }
+}

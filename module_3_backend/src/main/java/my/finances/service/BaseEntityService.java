33ce2ba468a0dda0e825d4139db99df4ba0a8704
Vitalik <frantsv2004@gmail.com>
2023-04-14 21:45:30 +0200

    add methods and fix bugs

--- module_3_backend/src/main/java/my/finances/service/BaseEntityService.java
+++ module_3_backend/src/main/java/my/finances/service/BaseEntityService.java
@@ -3,7 +3,6 @@ package my.finances.service;
 import my.finances.persistence.entity.BaseEntity;
 
 import java.util.Collection;
-import java.util.Optional;
 
 public interface BaseEntityService<ENTITY extends BaseEntity> {
     ENTITY findById(Long id);

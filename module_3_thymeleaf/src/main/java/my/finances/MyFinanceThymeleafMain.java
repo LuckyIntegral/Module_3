27caec05967ed7c65ee8b945045ebc4f02ca7831
Vitalik <frantsv2004@gmail.com>
2023-04-15 23:59:22 +0200

    add pom

---
+++ module_3_thymeleaf/src/main/java/my/finances/MyFinanceThymeleafMain.java
@@ -0,0 +1,11 @@
+package my.finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinanceThymeleafMain {
+    public static void main(String[] args) {
+        SpringApplication.run(MyFinanceThymeleafMain.class, args);
+    }
+}
